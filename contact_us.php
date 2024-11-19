acdb77be2b1141d115ea6fb5da8899881b03d061
Karuppaiya <karuppaiya1316@gmail.com>
2024-11-19 17:15:44 +0530

    first commit

---
+++ contact_us.php
@@ -0,0 +1,40 @@
+<div class="container">
+    <div class="clear-fix my-4"></div>
+    <div class="card card-outline card-primary rounded-0 shadow">
+        <div class="card-body py-5">
+            <h3 class="text-center font-weight-bolder">Our Company's Contact Information</h3>
+            <center>
+                <hr class="border-primary bg-primary w-25 border-3" style="opacity:1;height:3px;">
+            </center>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <dl>
+                        <dt><b>company_name :</b>  
+                            PKGK</dt>
+                        
+                        <dt><b>Email :</b> PKGK@sample.com</dt>
+                        
+                    </dl>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <dl>
+                        <dt><b>Telephone :</b> +456 789 1034</dt>
+                        <br>
+                        <br>
+                        
+                        <dt><b>Mobile :</b> 1234567890</dt>
+                        
+                    </dl>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <dl>
+                        <dt><b>Company Address : </b>Flat no:603, Maplewood , Chennai 603-103</dt>
+                        
+                        <dt><b>About Our Company :</b> Automated Question Builder</dt>
+                        
+                    </dl>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
